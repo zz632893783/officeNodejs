fccb7796790ef157bee04ed81f9c3f87856aa4b7
Zhang Zheng <632893783@qq.com>
2024-01-06 02:52:21 +0800

    233

---
+++ wordReader.js
@@ -0,0 +1,8 @@
+const mammoth = require('mammoth');  
+const fs = require('fs');  
+
+mammoth.convertToHtml({ path: './origin.docx' }).then(result => {
+    const html = result.value;
+    const messages = result.messages;
+    console.log(html, messages);
+});
